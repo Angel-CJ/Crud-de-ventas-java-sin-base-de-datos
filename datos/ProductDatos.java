6b32b14c98f3ce0435be2e1c65f33c576f4f19c1
ogangel <angel_tkr_16@hotmail.com>
2020-03-28 20:50:47 -0500

    Terminado

--- datos/ProductDatos.java
+++ datos/ProductDatos.java
@@ -1,28 +1,12 @@
 /*package datos;
-
-public class ProductDatos {
-    List<Cliente> listaProductos = new ArrayList<Cliente>();
-
-    public List<Cliente> list() {
-        return listaProductos;
-    }
-
-    public void create(Cliente c) {
-        listaProductos.add(c);
-        // System.out.println(d.getNombre()+" is created!");
-    }
-
-    public void delete(Cliente c) {
-        listaProductos.remove(c);
-    }
-
-    public void delete(int id2) {
+import ClienteDatos;
+public void remove(int id) {
         boolean existe = false;
-        for (Cliente c : listaProductos) {
-            System.out.println("Deleted:"+c.getId2() + "\t" + c.getNombre());
-            if (id2 == c.getId2()) {
+        for (Cliente c : listaClientes) {
+            System.out.println("Deleted:"+c.getId2() + "\t" + c.getProducto());
+            if (id == c.getId2()) {
                 try {
-                    listaProductos.remove(c);
+                    listaClientes.remove(c);
                     //listaContactos.update();
 
                 } catch (java.util.ConcurrentModificationException e2) {
